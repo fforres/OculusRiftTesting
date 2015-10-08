eaf4bb585182cafe5c383759cc15af3021fdc1f6
felipe.torressepulveda@gmail.com <felipe torres>
2015-10-08 01:10:03 -0300

    Fixing folders and creating map at loading time·

---
+++ Assets/_Scenes/Levels/Map1/Map Changer.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+var activeTerrain: Terrain;
+var activeTerrains: Terrain[];
+
+function Start () {
+	var terrainData = new TerrainData();
+	AssetDatabase.CreateAsset(terrainData, 'Assets/_Scenes/Levels/Map1/Terrain Flat.asset');
+	var newTerrain = Terrain.CreateTerrainGameObject(terrainData);
+	newTerrain.transform.position.x = 0;
+	newTerrain.transform.position.y = 0;
+	newTerrain.transform.position.z = 0;
+}
+
+function Update () {
+
+	activeTerrain = Terrain.activeTerrain;
+	activeTerrains = Terrain.activeTerrains;
+	
+	if(Time.realtimeSinceStartup > 5){
+	}
+}
--- Assets/_Scenes/common/Pause Menu/PauseMenu.js
+++ Assets/_Scenes/common/Pause Menu/PauseMenu.js
@@ -70,24 +70,26 @@ GUI.skin.button.font = pauseMenuFont;
 		//the user clicks on the "Change Graphics Quality" Button, and then dissapear when they click
 		//on it again....
 		if(showGraphicsDropDown == true){
+			
 			if(GUI.Button(Rect(Screen.width /2 + 150,Screen.height /2 ,250,50), "Fastest")){
-				QualitySettings.currentLevel = QualityLevel.Fastest;
+			//	QualitySettings.currentLevel = QualityLevel.Fastest;
 			}
 			if(GUI.Button(Rect(Screen.width /2 + 150,Screen.height /2 + 50,250,50), "Fast")){
-				QualitySettings.currentLevel = QualityLevel.Fast;
+			//	QualitySettings.currentLevel = QualityLevel.Fast;
 			}
 			if(GUI.Button(Rect(Screen.width /2 + 150,Screen.height /2 + 100,250,50), "Simple")){
-				QualitySettings.currentLevel = QualityLevel.Simple;
+			//	QualitySettings.currentLevel = QualityLevel.Simple;
 			}
 			if(GUI.Button(Rect(Screen.width /2 + 150,Screen.height /2 + 150,250,50), "Good")){
-				QualitySettings.currentLevel = QualityLevel.Good;
+			//	QualitySettings.currentLevel = QualityLevel.Good;
 			}
 			if(GUI.Button(Rect(Screen.width /2 + 150,Screen.height /2 + 200,250,50), "Beautiful")){
-				QualitySettings.currentLevel = QualityLevel.Beautiful;
+			//	QualitySettings.currentLevel = QualityLevel.Beautiful;
 			}
 			if(GUI.Button(Rect(Screen.width /2 + 150,Screen.height /2 + 250,250,50), "Fantastic")){
-				QualitySettings.currentLevel = QualityLevel.Fantastic;
+			//	QualitySettings.currentLevel = QualityLevel.Fantastic;
 			}
+			
 			
 			if(Input.GetKeyDown("escape")){
 				showGraphicsDropDown = false;
